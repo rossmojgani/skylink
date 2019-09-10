f758d13f06cbaae2d3619e1adc64ff367aea9e5a
Ross <rmojgani99@gmail.com>
2019-09-09 22:00:41 -0700

    working TCP skylink

--- testing_scripts/test.py
+++ testing_scripts/test.py
@@ -1,9 +1,9 @@
 from pymavlink import mavutil
 
-# Start a connection listening to a UDP port
-the_connection = mavutil.mavlink_connection('/dev/ttyACM0')
+# Start a connection listening to a TCP port exposed by SITL
+the_connection = mavutil.mavlink_connection('tcp:localhost:5760')
 
 # Wait for the first heartbeat
-#   This sets the system and component ID of remote system for the link
+# This sets the system and component ID of remote system for the link
 the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_system))
